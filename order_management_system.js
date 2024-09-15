cc4f83682d60d4aad67ada6c60046824ef4ce33c
Breanna Burris <breanna1burris@gmail.com>
2024-09-15 15:28:17 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,9 @@
+// Task 1: Inventory Array of Product Objects
+
+const inventory = [
+    { name: 'Espresso', price: 3, quantity: 10 },
+    { name: 'Latte', price: 4, quantity: 7 },
+    { name: 'Cappuccino', price: 4, quantity: 8 },
+    { name: 'Mocha', price: 5, quantity: 5 }
+]; //initializing inventory array
+
