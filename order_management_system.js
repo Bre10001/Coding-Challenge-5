1d080ccdcbbd6bb66c20020e9377fa24ef6226e4
Breanna Burris <breanna1burris@gmail.com>
2024-09-19 07:04:27 -0400

    Create placeOrder function

--- order_management_system.js
+++ order_management_system.js
@@ -9,3 +9,23 @@ const inventory = [
 
 // Task 2: Orders Array of Order Objects
 let orders = []; //initailize empty orders array
+
+// Task 3: Function to Place an Order
+function placeOrder(customerName, orderedItems){
+    let product = inventory.find(item => item.name === orderedItems); // using the find method to find the item
+    
+    if (product){
+        inventory.quantity -= orderedItems; // calculating the inventory available after sale
+        console.log(`Thank yoy for your order ${customerName}!`); 
+    } else {
+        console.log(`Error: ${orderedItems} is not in the inventory.`); //prints error if item not found 
+    };
+
+};
+
+orders.push({
+    customerName: customerName,
+    items: orderedItems,
+    status: "Pending"
+  });   // Add an order to the orders array with status 'Pending'
+    
